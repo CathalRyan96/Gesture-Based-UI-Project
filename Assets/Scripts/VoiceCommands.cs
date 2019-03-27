0a4d39e1b477deb94f1853e2baf167aa5fed6521
CathalRyan96 <cathalryan48@gmail.com>
2019-03-27 16:29:10 +0000

    voice commands now moving player

--- Assets/Scripts/VoiceCommands.cs
+++ Assets/Scripts/VoiceCommands.cs
@@ -11,18 +11,41 @@ public class VoiceCommands : MonoBehaviour {
 
     void Start()
     {
+        actions.Add("right", Right);
+        actions.Add("left", Left);
         actions.Add("up", Up);
         actions.Add("down", Down);
-        actions.Add("left", Left);
-        actions.Add("right", Right);
 
         keywordRecognizer = new KeywordRecognizer(actions.Keys.ToArray());
         keywordRecognizer.OnPhraseRecognized += SpeechRecognised;
+        keywordRecognizer.Start();
     }
 
     private void SpeechRecognised(PhraseRecognizedEventArgs speech)
     {
+        Debug.Log(speech.text);
+        actions[speech.text].Invoke();
 
     }
-	
+
+    private void Right()
+    {
+        transform.Translate(1, 0, 0);
+    }
+
+    private void Left()
+    {
+        transform.Translate(-1, 0, 0);
+    }
+
+    private void Up()
+    {
+        transform.Translate(0, 1, 0);
+    }
+
+    private void Down()
+    {
+        transform.Translate(0, -1, 0);
+    }
+
 }
